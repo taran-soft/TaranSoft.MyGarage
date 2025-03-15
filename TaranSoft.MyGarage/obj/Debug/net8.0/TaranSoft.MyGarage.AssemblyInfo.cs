3ebbc9e6d22cc058ec3c6d1f6e295d3f0559454d
Vladyslav Taran <taran.soft@gmail.com>
2025-03-15 13:00:33 +0100

    remove unnecessary files

--- TaranSoft.MyGarage/obj/Debug/net8.0/TaranSoft.MyGarage.AssemblyInfo.cs
+++ TaranSoft.MyGarage/obj/Debug/net8.0/TaranSoft.MyGarage.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaranSoft.MyGarage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb6ed5d70b99d3fb41002031586e928c3df9584f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaranSoft.MyGarage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaranSoft.MyGarage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
