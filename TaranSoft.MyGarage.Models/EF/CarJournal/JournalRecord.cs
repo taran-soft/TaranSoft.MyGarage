a67ecf87a470199a323612cff4d84824dd366359
Taran.Soft <taran.soft@gmail.com>
2025-05-10 15:11:55 +0200

    add first entities

---
+++ TaranSoft.MyGarage.Models/EF/CarJournal/JournalRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaranSoft.MyGarage.Data.Models.EF.CarJournal
+{
+    [Table("JournalRecords")]
+    public class JournalRecord : BaseEntity
+    {
+        [Required]
+        [ForeignKey(nameof(CarJournal.Id))]
+        public Guid JournalId { get; set; }
+    }
+}
