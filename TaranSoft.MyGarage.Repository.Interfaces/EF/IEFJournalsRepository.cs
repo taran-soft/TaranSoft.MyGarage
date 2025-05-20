75ebee341bd965c45db2c20f621082c2480a38cc
Taran.Soft <taran.soft@gmail.com>
2025-05-20 19:42:50 +0200

    add journals service and repository

---
+++ TaranSoft.MyGarage.Repository.Interfaces/EF/IEFJournalsRepository.cs
@@ -0,0 +1,12 @@
+﻿using TaranSoft.MyGarage.Data.Models.EF.CarJournal;
+
+namespace TaranSoft.MyGarage.Repository.Interfaces.EF;
+
+public interface IEFJournalsRepository
+{
+    Task<ICollection<Journal>> GetByVehicleId(long id);
+
+    Task AddAsync(Journal journal);
+    Task UpdateAsync(Journal journal);
+    Task DeleteAsync(long id);
+}
---
+++ TaranSoft.MyGarage.Services.Interfaces/IJournalsService.cs
@@ -0,0 +1,8 @@
+﻿using TaranSoft.MyGarage.Contracts.Dto;
+
+namespace TaranSoft.MyGarage.Services.Interfaces;
+
+public interface IJournalsService
+{
+    Task<ICollection<JournalDto>> GetByVehicleId(long id);
+}
